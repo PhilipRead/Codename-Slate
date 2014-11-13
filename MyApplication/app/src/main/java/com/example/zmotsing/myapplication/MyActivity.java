d491a051959fcbcd4d42a19d7e65d7e3005b4bfd
acowdrey <cowdrey@ku.edu>
2014-11-12 20:57:24 -0600

    Add some files for openGL support.

--- MyApplication/app/src/main/java/com/example/zmotsing/myapplication/MyActivity.java
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/MyActivity.java
@@ -2,37 +2,47 @@ package com.example.zmotsing.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.ImageView;
+import android.opengl.GLSurfaceView;
 
 public class MyActivity extends Activity {
-    float oldX;
-    float oldY;
+//    float oldX;
+//    float oldY;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_my);
+//
+//        final ImageView imageView = (ImageView)findViewById(R.id.redSquare);
+//        imageView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent event) {
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        oldX = event.getX();
+//                        oldY = event.getY();
+//                        return true;
+//                    case MotionEvent.ACTION_MOVE:
+//                        imageView.setX(event.getRawX() - oldX);
+//                        imageView.setY(event.getRawY() - oldY);
+//                        return true;
+//                    case MotionEvent.ACTION_UP:
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+
+    private GLSurfaceView mGLView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
 
-        final ImageView imageView = (ImageView)findViewById(R.id.redSquare);
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        oldX = event.getX();
-                        oldY = event.getY();
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        imageView.setX(event.getRawX() - oldX);
-                        imageView.setY(event.getRawY() - oldY);
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                }
-                return false;
-            }
-        });
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
     }
 }
