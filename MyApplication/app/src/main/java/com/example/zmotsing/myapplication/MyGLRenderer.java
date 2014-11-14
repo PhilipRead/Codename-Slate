6af1b4b6210effe09d5d814cdb4ac68a45b7a36e
acowdrey <cowdrey@ku.edu>
2014-11-14 17:45:12 -0600

    Bouncing Triangle

--- MyApplication/app/src/main/java/com/example/zmotsing/myapplication/MyGLRenderer.java
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/MyGLRenderer.java
@@ -1,7 +1,6 @@
 package com.example.zmotsing.myapplication;
 
 import android.annotation.TargetApi;
-import android.opengl.EGLConfig;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
@@ -18,19 +17,18 @@ import javax.microedition.khronos.opengles.GL10;
  */
 @TargetApi(Build.VERSION_CODES.KITKAT)
 public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+
     private FloatBuffer triangleVB;
 
-    private final String vertexShaderCode = "attribute vec4 vPosition; \n"
-            + "void main(){              \n" + " gl_Position = vPosition; \n"
-            + "}                         \n";
+    private final String vertexShaderCode = "attribute vec4 vPosition; \n"+ "void main(){              \n" + " gl_Position = vPosition; \n"+ "}                         \n";
 
-    private final String fragmentShaderCode = "precision mediump float;  \n"
-            + "void main(){              \n"
-            + " gl_FragColor = vec4 (0.63671875, 0.76953125, 0.22265625, 1.0); \n"
-            + "}                         \n";
+    private final String fragmentShaderCode = "precision mediump float;  \n"+ "void main(){              \n" + " gl_FragColor = vec4 (0.63671875, 0.76953125, 0.22265625, 1.0); \n" + "}                         \n";
 
     private int mProgram;
-    private int maPositionHandle;
+    private float fun=0;
+    private int bobble = 1;
+    private int mPositionHandle;
 
     private int loadShader(int type, String shaderCode) {
 
@@ -45,17 +43,31 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         return shader;
     }
 
+    private float[] mRotationMatrix = new float[16];
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
         GLES20.glUseProgram(mProgram);
 
-        GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT,
-                false, 12, triangleVB);
-        GLES20.glEnableVertexAttribArray(maPositionHandle);
+        GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false, 12, triangleVB);
+
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+        fun+= .03 *bobble;
+        if(Math.abs(fun) > .5)
+        {
+            bobble *= -1;
+        }
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        //Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+        // Draw triangle
+        initShapes();
     }
 
 
@@ -74,17 +86,19 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glAttachShader(mProgram, fragmentShader);
         GLES20.glLinkProgram(mProgram);
 
-        maPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
     }
 
     private void initShapes() {
         float[] triangleCoords = new float[]{
                 // X,Y,Z
-                -0.5f, -0.25f, 0, 0.5f, -0.25f, 0, 0.0f, 0.559016994f, 0};
+                -0.5f, -0.25f, 0, 0.5f, -0.25f, 0, 0.0f + fun, 0.559016994f, 0};
         ByteBuffer vbb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
         vbb.order(ByteOrder.nativeOrder());
         triangleVB = vbb.asFloatBuffer();
         triangleVB.put(triangleCoords);
         triangleVB.position(0);
     }
+
+
 }
