7dd8d3ad658fa21bb018f5e756969ceb153d24d2
Aaron Cowdrey <cowdrey@ku.edu>
2014-11-29 00:07:26 -0600

    Added Spline functionality. Added a Line Strip class that will take a list of coordinates and create a multiple line segment.

--- MyApplication/app/src/main/java/com/example/zmotsing/myapplication/MyGLRenderer.java
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/MyGLRenderer.java
@@ -1,10 +1,13 @@
 package com.example.zmotsing.myapplication;
 
 import android.annotation.TargetApi;
+import android.graphics.Camera;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
+import android.opengl.Matrix;
 import android.os.Build;
-
+import android.util.Log;
+import javax.microedition.khronos.opengles.GL10;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +19,18 @@ import javax.microedition.khronos.opengles.GL10;
  */
 @TargetApi(Build.VERSION_CODES.KITKAT)
 public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+
+    /**
+     * Keeps track of the Projection matrix calculated on the last draw frame
+     */
+    private float[] lastProjectionMat = null;
+
+    /**
+     * Keeps track of the model view matrix calculated on the last frame
+     */
+    private float[] lastModelViewMat = null;
+
 
     private final String vertexShaderCode = "attribute vec4 vPosition; \n"
             + "void main(){              \n" + " gl_Position = vPosition; \n"
@@ -29,17 +44,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private int mProgram;
     private int maPositionHandle;
 
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * android.opengl.GLSurfaceView.Renderer#onDrawFrame(javax.
-         * microedition.khronos.opengles.GL10)
-     */
-// Initialize our square.
-    LineStrip linestrip;
-    List<Coord> controlPoints = new ArrayList<>();
+    static LineStrip linestrip;
+    static List<Coord> controlPoints = new ArrayList<>();
 
     @Override
     public void onDrawFrame(GL10 gl) {
@@ -50,7 +56,10 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Draw our square.
         // Translates 4 units into the screen.
         gl.glTranslatef(0, 0, -4);
-        linestrip.draw(gl); // ( NEW )
+        if(linestrip != null) {
+            linestrip.draw(gl); // ( NEW )
+
+        }
         // Replace the current matrix with the identity matrix
         gl.glLoadIdentity();
     }
@@ -79,6 +88,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         // Reset the modelview matrix
         gl.glLoadIdentity();
+
     }
     @Override
     public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig eglConfig) {
@@ -97,7 +107,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
                 GL10.GL_NICEST);
 
 
-        controlPoints.add(new Coord(-0.6f,  0.6f));
+        this.lastProjectionMat = new float[16];
+        this.lastModelViewMat = new float[16];
         controlPoints.add(new Coord(-0.6f,  -0.6f));
         controlPoints.add(new Coord(0.6f,  -0.6f));
         controlPoints.add(new Coord(0.6f,  0.6f));
@@ -105,5 +116,37 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         controlPoints.add(new Coord(0.2f,  -1f));
         linestrip = new LineStrip(Spline.interpolate(controlPoints,60,CatmullRomType.Chordal));
     }
+    public static void addControlPoints(float x, float y)
+    {
+        controlPoints.add(new Coord(x,y));
+        if(controlPoints.size() > 2) {
+            linestrip = new LineStrip(Spline.interpolate(controlPoints, 60, CatmullRomType.Chordal));
+        }
+    }
+    /**
+     * Calculates the transform from screen coordinate
+     * system to world coordinate system coordinates
+     * for a specific point, given a camera position.
+     *
+     * @param touch Coord point of screen touch, the
+    actual position on physical screen (ej: 160, 240)
+     * @return position in WCS.
+     */
+    public Coord GetWorldCoords(Coord touch)
+    {
+        Coord worldPos;
 
+        // SCREEN height & width (ej: 320 x 480)
+        float screenW = MyActivity.width;
+        float screenH = MyActivity.height;
+        float oglTouchY = screenH - touch.Y;
+
+       /* Transform the screen point to clip space in ogl (-1,1) */
+        float glWidth = (float) ((touch.X) * 6f / screenW - 3);
+        float glHeight = (float) ((oglTouchY) * 3.2f / screenH -1.5);
+
+        worldPos = new Coord(glWidth, glHeight);
+
+        return worldPos;
+    }
 }
