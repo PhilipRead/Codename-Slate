462b56495f83a2229ec347fb366f3561b2a72826
Zach Motsinger <zam2007@ku.edu>
2015-03-09 21:34:04 -0500

    Extending backend logic

---
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/Backend/OutputNode.java
@@ -0,0 +1,12 @@
+package com.example.zmotsing.myapplication.Backend;
+
+/**
+ * Created by zmotsing on 3/9/15.
+ */
+public class OutputNode extends BackendNode
+{
+    OutputNode(String id)
+    {
+        super(id);
+    }
+}
