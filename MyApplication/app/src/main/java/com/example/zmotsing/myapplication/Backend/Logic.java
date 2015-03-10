700496cd741b71f6fcc9db083f0f217151307e2e
philipread <4Lights!>
2015-03-10 11:47:17 -0500

    Added Back Removed Files

---
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/Backend/Logic.java
@@ -0,0 +1,72 @@
+package com.example.zmotsing.myapplication.Backend;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Created by zmotsing on 12/11/14.
+ */
+public class Logic {
+    private CopyOnWriteArrayList<VariableNode> logicNodes = new CopyOnWriteArrayList<>(); //list of backend nodes
+
+    //default constructor
+    public Logic() {
+        //test code to be deleted later
+        addVariableNode("x", 5);
+        execute();
+        updateVariableNode("+", "x", 5);
+        execute();
+    }
+
+    //create a new variable node
+    public void addVariableNode(String name, int value) {
+        VariableNode newNode = new VariableNode(name, value);
+        logicNodes.add(newNode);
+    }
+
+    //update variable node
+    public void updateVariableNode(String operation, String name, int value) {
+        VariableNode node = new VariableNode();
+        boolean found = false;
+        for (VariableNode nd : logicNodes) {
+            if (nd.getName().equals(name)) {
+                node = nd;
+                found = true;
+                break;
+            }
+        }
+
+        //error
+        if (!found) {
+            return;
+        }
+
+        switch (operation) {
+            case "+":
+                node.setValue(node.getValue() + value);
+                break;
+            case "-":
+                node.setValue(node.getValue() - value);
+                break;
+            case "*":
+                node.setValue(node.getValue() * value);
+                break;
+            case "/":
+                node.setValue(node.getValue() / value);
+                break;
+            default:
+                node.setValue(value);
+                break;
+        }
+    }
+
+    //iterate through the backend nodes
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    public void execute() {
+        for (VariableNode nd : logicNodes) {
+            System.out.println(nd.output());
+        }
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/Backend/VariableNode.java
@@ -0,0 +1,34 @@
+package com.example.zmotsing.myapplication.Backend;
+
+/**
+ * Created by zmotsing on 12/11/14.
+ */
+public class VariableNode {
+    private String name;
+    private int value;
+
+    public VariableNode() {
+    }
+
+    public VariableNode(String n, int v) {
+        name = n;
+        value = v;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int v) {
+        value = v;
+    }
+
+    //return string representation of the variable and its value
+    public String output() {
+        return (name + " : " + value);
+    }
+}
