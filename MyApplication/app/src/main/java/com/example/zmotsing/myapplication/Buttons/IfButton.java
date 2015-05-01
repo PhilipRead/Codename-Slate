584f33dca23b15a5270d36b54fb404966eb0d4b1
Aaron Cowdrey <cowdrey@ku.edu>
2015-05-01 13:43:36 -0500

    Shifted methods around, added some regions for readability

--- MyApplication/app/src/main/java/com/example/zmotsing/myapplication/Buttons/IfButton.java
+++ MyApplication/app/src/main/java/com/example/zmotsing/myapplication/Buttons/IfButton.java
@@ -3,6 +3,7 @@ package com.example.zmotsing.myapplication.Buttons;
 import com.example.zmotsing.myapplication.Coord;
 import com.example.zmotsing.myapplication.MyGLSurfaceView;
 import com.example.zmotsing.myapplication.Node;
+import com.example.zmotsing.myapplication.NodeType;
 import com.example.zmotsing.myapplication.R;
 import com.example.zmotsing.myapplication.Sprite;
 
@@ -23,6 +24,7 @@ public class IfButton extends Node {
 
     @Override
     public void action(MyGLSurfaceView SV) {
+        com.example.zmotsing.myapplication.MyGLRenderer.nodeTypeCreate= NodeType.IF;
 
     }
 
